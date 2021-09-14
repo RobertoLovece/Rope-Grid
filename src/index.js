386980e70b5f19a010012ddd188ce900205da1aa
rlovece <rlovece@hotmail.co.uk>
2021-09-14 18:53:36 +0100

    Initialised based off Three.js-Line

---
+++ src/index.js
@@ -0,0 +1,143 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+import Line from './utilities/line.js'
+import {initGui} from './utilities/gui.js'
+
+require('normalize.css/normalize.css');
+require("./index.css");
+
+let renderer, scene, camera;
+let container, controls;
+let raycaster, mouse, selectedObject;
+let line;
+
+window.onload = function () {
+    
+    init();
+    initObjects();
+
+    initGui(line);
+
+    initControls();
+    // initRaycaster();
+
+    animate();
+
+    initEventListeners();
+    onWindowResize();
+}
+
+function init() {
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+
+    container = document.getElementById('canvas');
+    container.appendChild(renderer.domElement);
+
+    camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.set(- 40, 0, 60);
+
+    scene = new THREE.Scene();
+
+}
+
+function initObjects() {
+
+    var points = [];
+    points.push(new THREE.Vector3(15, 0, 0))
+    points.push(new THREE.Vector3(-15, 0, 0))
+
+    line = new Line(points, 5, false);
+    scene.add(line);
+
+}
+
+function initControls() {
+
+    controls = new OrbitControls(camera, renderer.domElement);
+
+    controls.minDistance = 10;
+    controls.maxDistance = 150;
+    controls.enablePan = false;
+}
+
+function initRaycaster() {
+
+    raycaster = new THREE.Raycaster();
+    raycaster.params.Line.threshold = 0.02; 
+    mouse = new THREE.Vector2(30, 30);
+
+    selectedObject = null;
+
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    // renderer will set this eventually
+    // line.matLine.resolution.set(window.innerWidth, window.innerHeight); // resolution of the viewport
+    line.matLine.resolution.set(container.offsetWidth, container.offsetHeight); // resolution of the viewport
+
+    renderer.render(scene, camera);
+
+}
+
+function initEventListeners() {
+    window.addEventListener('resize', onWindowResize, false);
+    // document.addEventListener('pointermove', onPointerMove);
+}
+
+/*
+ *  CODE BELOW IS FOR EVENT LISTENERS 
+ */
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+// modification of code from {https://github.com/mrdoob/three.js/blob/master/examples/webgl_raycast_sprite.html}
+function onPointerMove( event ) {
+
+    if ( selectedObject ) {
+
+        // set back to default colour and clear selected object
+        selectedObject.material.color.set(0xffffff);
+        selectedObject = null;
+
+    }
+
+    // mouse position for comparision to object positions
+	// trueMouse.x = (( event.clientX / window.innerWidth ) * canvasW) - canvasW/2; 
+	// trueMouse.y = -((( event.clientY / window.innerHeight ) * canvasH) - canvasH/2);
+
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+    raycaster.setFromCamera( mouse, camera );
+
+    const intersects = raycaster.intersectObjects( scene.children);
+
+    if ( intersects.length > 0 ) {
+
+        const res = intersects.filter( function ( res ) {
+
+            return res && res.object;
+
+        } )[ 0 ];
+
+        if ( res && res.object ) {
+
+            // set the object to red when highlighted
+            selectedObject = res.object;
+            selectedObject.material.color.set(0xff0000);
+
+        }
+
+    }
+
+}
